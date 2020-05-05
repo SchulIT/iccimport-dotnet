a6a6b0c7b3e4f335a7c38f9831173c849087eee3
Marcel Marnitz <marcelmarnitz@googlemail.com>
2020-05-05 21:36:23 +0200

    Fixes RoomData

--- IccImport/Models/RoomData.cs
+++ IccImport/Models/RoomData.cs
@@ -4,11 +4,14 @@ namespace SchulIT.IccImport.Models
 {
     public class RoomData
     {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
         [JsonProperty("name")]
         public string Name { get; set; }
 
-        [JsonProperty("long_name")]
-        public string LongName { get; set; }
+        [JsonProperty("description")]
+        public string Description { get; set; }
 
         [JsonProperty("capacity")]
         public int? Capacity { get; set; }
